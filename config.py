b047a0b99e1461fc587e85c3b5cc0a3b346febcc
Aleksandr Bundych <127985523+alastar-tech@users.noreply.github.com>
2024-12-18 19:21:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,3 @@
-TOKEN = '7602572065:AAHZFlig4ut2JDj8j93XCYXKozogLqQ1s5c'
+TOKEN = 'bot_token'
 
-ADMINS = [6022714907, 782527150]
-
-'''
-db_conf = { 'user' : 'postgres',
-            'psw' : '321321321',
-            'database' : 'nurse_check_bot',
-            'host' : '213.108.21.50',
-            'port' : 5432}
-'''
+ADMINS = [user_id_1, user_id_2]
